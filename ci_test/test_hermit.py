4bd31dbb075c4831e67c704fce06170aa60d5da7
Hervé Ménager <herve.menager@gmail.com>
2020-05-13 18:10:42 +0200

    a few more improvements to edamxpathvalidator/hermit tests

--- ci_test/test_hermit.py
+++ ci_test/test_hermit.py
@@ -1,5 +1,7 @@
 import unittest
-import logging, sys
+import logging
+import os
+import sys
 import subprocess
 
 import nbformat
@@ -11,18 +13,12 @@ HERMIT_OK_STRING="Classes equivalent to 'owl:Nothing':\n\towl:Nothing\n"
 
 class TestLogging(unittest.TestCase):
 
-    def setUp(self) -> None:
-        logging.info("Logger initialized ! ")
-
     def test_hermit(self):
-        process = subprocess.run(['java', '-jar', 'HermiT.jar', '-U', '../../edamontology/EDAM_dev.owl'], stdout=subprocess.PIPE, universal_newlines=True)
+        edam_path = os.environ.get('EDAM_PATH', 'EDAM_dev.owl')
+        process = subprocess.run(['java', '-jar', 'HermiT.jar', '-U', edam_path], stdout=subprocess.PIPE, universal_newlines=True)
         self.assertEqual(process.stdout, HERMIT_OK_STRING, msg='HermiT reasoner detected some inconsistencies in the ontology')
         if process.returncode != 0:
             self.fail(f'HermiT reasoner failed with rc {process.returncode}')
-
-    def tearDown(self) -> None:
-        logging.info("End of unit test")
-
 
 if __name__ == '__main__':
     unittest.main()
