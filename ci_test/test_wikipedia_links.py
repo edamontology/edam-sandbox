3eaca8f825f6bf6cd0d9d320966687adb98b6442
Alban Gaignard <alban.gaignard@univ-nantes.fr>
2021-10-18 12:18:26 +0200

    sample testing script

---
+++ ci_test/test_wikipedia_links.py
@@ -0,0 +1,42 @@
+import unittest
+import logging, sys
+from parameterized import parameterized
+import glob
+import os
+import json
+
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s: %(message)s', stream=sys.stdout)
+
+
+class TestNotebooks(unittest.TestCase):
+
+
+    def setUp(self) -> None:
+        logging.info("Logger intialized ! ")
+
+
+    @parameterized.expand([entry_path for entry_path in glob.glob(os.path.join(".","wikipedia_links.ipynb"))])
+    def test_all_nb(self, entry_path):
+        #for nb in entry_path:
+            print(f'Testing {entry_path}')
+
+            with open(entry_path) as f:
+                nb = nbformat.read(f, as_version=4)
+                ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
+                ep.preprocess(nb, {'metadata': {'path': '.'}})
+
+                edam_test_output = nb['cells'][-1]['outputs'][0]['text']
+                print(edam_test_output)
+                #out = json.loads(edam_test_output)
+                #print(f"test name: {out['test_name']}\nstatus: {out['status']}\nreason: {out['reason']}")
+                self.assertFalse("error" in edam_test_output.lower())
+
+    def tearDown(self) -> None:
+        logging.info("End of unit test")
+
+
+if __name__ == '__main__':
+    unittest.main()
